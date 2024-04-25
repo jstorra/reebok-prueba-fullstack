0e30b0b7615dc2d7ff0d6ca0427cc1972bb0780a
jstorra <131458634+jstorra@users.noreply.github.com>
2024-04-25 15:43:27 -0500

    feat: :sparkles: Actualizando backend

--- backend/src/main/java/jstorra/backend/cors/CorsConfig.java
+++ backend/src/main/java/jstorra/backend/cors/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*")
+                registry.addMapping("/**")
                         .allowedOrigins("http://localhost:8080", "http://127.0.0.1:5500")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
